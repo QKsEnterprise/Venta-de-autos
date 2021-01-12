db1976b8dc35b7e1f247970a5c1b0f0a8d135963
Kuka <martinelliluca@gmail.com>
2021-01-12 18:02:58 -0300

    agruegue un sistema de pago

---
+++ transactionWithCreditCard.js
@@ -0,0 +1,102 @@
+let Ferrari1 = 'modelo de auto: Ferrari 488 spider'
+
+//console.log(Ferrari1);
+
+let Ferrari2 = 'car model: Ferrari 488 Gtb'
+
+//console.log(Ferrari2);
+
+let Ferrari3 = 'car model: Ferrari 355 3.5 Berlintta'
+
+//console.log(Ferrari3);
+
+let Ferrari4 = 'car model: Ferrari California 4.3 510cv'
+
+//console.log(Ferrari4);
+
+let Ferrari5 = 'car model: Ferrari 458 4.5 Spider'
+
+//console.log(Ferrari5);
+
+
+
+
+//Valores en precio
+
+
+
+let ferrari1 = 999989
+
+//console.log(ferrari1);
+
+let ferrari2 = 999998
+
+//console.log(ferrari2);
+
+let ferrari3 = 265000
+
+//console.log(ferrari3);
+
+let ferrari4 = 299999
+
+//console.log(ferrari4);
+
+let ferrari5 = 699000
+
+//console.log(ferrari5);
+
+
+
+
+
+//Validacion de la tarjeta de credito
+
+
+
+
+let TypeCard = 'Credit Card';
+
+switch (TypeCard) {
+    case ('Credit Card'):
+        console.log('This is a Credit Card');
+             break;
+        default:
+                ('No Card');
+}
+
+
+
+
+
+//Metodos  de pago
+
+
+
+
+
+console.log(Ferrari2);
+
+console.log('cost: $', ferrari2); 
+
+let typecard = 'Credit Card';
+
+typecard = 10000000;
+
+let subtrack = typecard - ferrari2;
+
+if (typecard >= ferrari2) {
+        console.log('Transaction ¡Ok!');
+            console.log('Now you have: $',subtrack);
+}
+        else {
+            console.log('Fail Transaction')
+}
+
+typecard = subtrack
+
+    //console.log(typecard)
+
+
+
+
+
