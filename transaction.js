53ca4d8c591772ca3eb767205b5a6594b9307dfe
Kuka <martinelliluca@gmail.com>
2021-01-12 17:18:47 -0300

    agruegue los valores de los autos

--- transaction.js
+++ transaction.js
@@ -1,26 +1,61 @@
 let Ferrari1 = ['Ferrari 488 spider'];
 
-console.log(Ferrari1);
+//console.log(Ferrari1);
 
 let Ferrari2 = ['Ferrari 488 Gtb'];
 
-console.log(Ferrari2);
+//console.log(Ferrari2);
 
 let Ferrari3 = ['Ferrari 355 3.5 Berlintta']
 
-console.log(Ferrari3);
+//console.log(Ferrari3);
 
 let Ferrari4 =  ['Ferrari California 4.3 510cv']
 
-console.log(Ferrari4);
+//console.log(Ferrari4);
 
 let Ferrari5 = ['Ferrari 458 4.5 Spider']
 
-console.log(Ferrari5);
+//console.log(Ferrari5);
 
-let typecard = 'Credit Card';
 
-typecard = 10000000;
+
+
+//Valores en precio
+
+
+
+let ferrari1 = ['$999989']
+
+//console.log(ferrari1);
+
+let ferrari2 = ['$999998']
+
+//console.log(ferrari2);
+
+let ferrari3 = ['$265000']
+
+//console.log(ferrari3);
+
+let ferrari4 = ['$299999']
+
+//console.log(ferrari4);
+
+let ferrari5 = ['$699000']
+
+//console.log(ferrari5);
+
+
+
+//Metodos  de pago
+
+console.log(Ferrari2)
+
+console.log(ferrari2) 
+
+
+
+
 
 
 
