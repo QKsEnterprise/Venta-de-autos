95abb65182eb49c9965da53cb27e7d4d2292ba7c
Kuka <martinelliluca@gmail.com>
2021-01-12 16:55:54 -0300

    agregue modelos de autos

--- cards.js
+++ cards.js
@@ -1,12 +1,12 @@
-let typeCard = 'Debit Card';
+//let typeCard = 'Debit Card';
 
-switch (typeCard) {
-   case ('Debit Card'):
-       document.write('This is a Debit Card');
-            break;
-        default:
-            document.write('No Card');
-};
+//switch (typeCard) {
+  // case ('Debit Card'):
+      // document.write('This is a Debit Card');
+           // break;
+        //default:
+           // document.write('No Card');
+;
 
 //let TypeCard = 'Credit Card';
 
---
+++ transaction.js
@@ -0,0 +1,26 @@
+let Ferrari1 = ['Ferrari 488 spider'];
+
+console.log(Ferrari1);
+
+let Ferrari2 = ['Ferrari 488 Gtb'];
+
+console.log(Ferrari2);
+
+let Ferrari3 = ['Ferrari 355 3.5 Berlintta']
+
+console.log(Ferrari3);
+
+let Ferrari4 =  ['Ferrari California 4.3 510cv']
+
+console.log(Ferrari4);
+
+let Ferrari5 = ['Ferrari 458 4.5 Spider']
+
+console.log(Ferrari5);
+
+let typecard = 'Credit Card';
+
+typecard = 10000000;
+
+
+
