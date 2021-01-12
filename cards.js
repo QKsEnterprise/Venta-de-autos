f19443997ce7acde6994924c0b499c98ce4bc429
Kuka <martinelliluca@gmail.com>
2021-01-12 14:05:12 -0300

    agregue un programa para indentificar tarjetas

---
+++ cards.js
@@ -0,0 +1,14 @@
+let typeCard = 'Debit Card';
+
+let TypeCard = 'Credit Card';
+
+switch (typeCard){
+   case ('Debit Card'):
+       document.write('This is a Debit Card')
+            break;
+    case ('Credit Card'):
+        document.write('this is a Credit Card')
+             break;
+        default:
+            document.write('No Card')
+};
